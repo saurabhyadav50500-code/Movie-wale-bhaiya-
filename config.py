c8385321bd750477ffa1947ac23acbe4a2389164
saurabhyadav50500-code <saurabhyadav50500@gmail.com>
2026-01-08 18:04:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-API_ID =  34119293
-API_HASH = "ebea85e712484252c681be91348fdc85"  
-BOT_TOKEN = "8049427229:AAE_MuHSbRJ2TeVD_TPEOBnHRk8fxRc9zik"
+# config.py
+
+API_ID = 12345678  # यहाँ अपना API ID डालें (Integers में)
+API_HASH = "yOuRApiHaShHeRe"  # यहाँ अपना API HASH डालें
+BOT_TOKEN = "123456:ABC-DefGhIjKlMnOpQrStUvWxYz"  # यहाँ अपना Bot Token डालें
