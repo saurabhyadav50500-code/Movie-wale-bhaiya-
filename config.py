a386b7cec1f6953502157fba0e73089a6b9da6cd
saurabhyadav50500-code <saurabhyadav50500@gmail.com>
2026-01-08 21:01:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-API_ID = 12345678  # यहाँ अपना API ID डालें (Integers में)
-API_HASH = "yOuRApiHaShHeRe"  # यहाँ अपना API HASH डालें
-BOT_TOKEN = "123456:ABC-DefGhIjKlMnOpQrStUvWxYz"  # यहाँ अपना Bot Token डालें
+API_ID = 1234567  # Yahan apna API ID dalein (Integer hona chahiye)
+API_HASH = "aapka_api_hash_yahan" # Yahan apna API HASH dalein
+BOT_TOKEN = "aapka_bot_token_yahan" # BotFather se mila hua token yahan dalein
