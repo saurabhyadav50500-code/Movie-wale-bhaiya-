501eaa49d1a4eda5ade4e967d41c231067af8acd
saurabhyadav50500-code <saurabhyadav50500@gmail.com>
2026-01-11 01:11:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,6 +5,6 @@ API_ID = int(os.environ.get("API_ID", "34119293"))
 API_HASH = os.environ.get("API_HASH", "ebea85e712484252c681be91348fdc85")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7956005223:AAGlqJdNnOizY-ijdcjldQsVEFYYP9LojPs")
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Sur67:Sur67@cluster0.qh4c56q.mongodb.net/?retryWrites=true&w=majority")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "847367333, 1729007340").split()]
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "847367333 1729007340").replace(',', ' ').split()]
 DATABASE_NAME = "Cluster0"
 COLLECTION_NAME = "files"
