4d4ab20b9df5f92fce94803f59395eb8d38eda92
saurabhyadav50500-code <saurabhyadav50500@gmail.com>
2026-01-10 22:47:06 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-
-API_ID = 34119293  # Yahan apna API ID dalein (Integer hona chahiye)
-API_HASH = "ebea85e712484252c681be91348fdc85" # Yahan apna API HASH dalein
-BOT_TOKEN = "7956005223:AAGlqJdNnOizY-ijdcjldQsVEFYYP9LojPs" # BotFather se mila hua token yahan dalein
