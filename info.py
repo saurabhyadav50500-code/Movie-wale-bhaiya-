8ce523120c73e8d1e216c884b78dc32a8822ba92
saurabhyadav50500-code <saurabhyadav50500@gmail.com>
2026-01-10 23:59:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 # info.py
 import os
 
-API_ID = int(os.environ.get("API_ID", "12345"))
-API_HASH = os.environ.get("API_HASH", "your_hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
-MONGO_URI = os.environ.get("MONGO_URI", "your_mongo_uri")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "12345678").split()]
+API_ID = int(os.environ.get("API_ID", "34119293"))
+API_HASH = os.environ.get("API_HASH", "ebea85e712484252c681be91348fdc85")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8049427229:AAE_MuHSbRJ2TeVD_TPEOBnHRk8fxRc9zik")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Sur67:Sur67@cluster0.qh4c56q.mongodb.net/?retryWrites=true&w=majority")
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "847367333").split()]
 DATABASE_NAME = "Cluster0"
 COLLECTION_NAME = "files"
