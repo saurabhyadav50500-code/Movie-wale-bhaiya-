8bfefb4adce0f4860890f0c643d2e2a081e7c043
saurabhyadav50500-code <saurabhyadav50500@gmail.com>
2026-01-14 21:53:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "34119293"))
 API_HASH = os.environ.get("API_HASH", "ebea85e712484252c681be91348fdc85")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7956005223:AAGlqJdNnOizY-ijdcjldQsVEFYYP9LojPs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7884415007:AAGbQXJk01qv5gMMy7nM2YB9rU5z41_aedU")
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Sur67:Sur67@cluster0.qh4c56q.mongodb.net/?retryWrites=true&w=majority")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "847367333 1729007340").replace(',', ' ').split()]
 DATABASE_NAME = "Cluster0"
